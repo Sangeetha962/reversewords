cce3a6677fac710cb8f3c9e0e42ff3842d42cb17
Sangeetha962 <geethasangeetha.1321@gmail.com>
2025-12-27 14:43:53 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,9 @@
+def reversewords(s):
+        s=s.split()
+        out=[]
+        for i in range(len(s)):
+            out.append(s.pop())
+        return " ".join(out)   
+        
+s=input("Enter the string:")
+print(reversewords(s))
